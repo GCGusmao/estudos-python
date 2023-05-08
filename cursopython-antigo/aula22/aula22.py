f4d0e9e35bb3c01f39b2cfcc5004a333f022f1d3
GCGusmao <69112221+GCGusmao@users.noreply.github.com>
2023-05-08 18:26:46 -0300

    Andamento do curso.

--- cursopython-antigo/aula22/aula22.py
+++ cursopython-antigo/aula22/aula22.py
@@ -5,7 +5,7 @@ msg = 'Usuário logado.' if logged_user else 'Usuário precisa fazer o loggin.'
 
 print(msg)
 
-digito_10 = 18
+digito_10 = 8
 condicao_1 = (digito_10 <= 9)
 
 digito_10 = digito_10 if condicao_1 else 0
