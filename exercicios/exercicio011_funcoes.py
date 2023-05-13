7ec7b602ba0a32bbbbb138d9a851474733fc48c3
GCGusmao <69112221+GCGusmao@users.noreply.github.com>
2023-05-13 20:34:44 -0300

    Andamento curso.

---
+++ exercicios/exercicio011_funcoes.py
@@ -0,0 +1,11 @@
+#  Exercício com funções
+
+#  Crie uma função que multiplica todos os argumentos
+#  não nomeados recebidos
+#  Retorne o total para uma variável e mostre o valor
+#  da variável.
+
+
+#  Crie uma função que fala se o número é par ou impar
+#  Retorne se o número informado é par ou impar.
+#
