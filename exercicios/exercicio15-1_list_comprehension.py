759c19fb620241d40212c8b37b933f47d9a6e724
GCGusmao <69112221+GCGusmao@users.noreply.github.com>
2023-06-20 20:58:13 -0300

    Andamento curso.

---
+++ exercicios/exercicio15-1_list_comprehension.py
@@ -0,0 +1,10 @@
+
+name_list = [
+    'gustavo', 'larissa', 'adriana', 'flavio', 'lucia', 'edvaldo', 'ariely'
+]
+
+print(name_list)
+
+result = [item+' Gusmão' for item in name_list]
+
+print(result)
