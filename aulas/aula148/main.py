1078aea90a7dda7d1e4afb1abd83a2cea2ed470d
GCGusmao <69112221+GCGusmao@users.noreply.github.com>
2023-07-24 20:19:04 -0300

    andamento curso.

---
+++ aulas/aula148/main.py
@@ -0,0 +1,8 @@
+# import uma_linha
+#import varias_linhas
+import documentacao_class
+
+# help(uma_linha)
+
+# help(varias_linhas)
+help(documentacao_class)
---
+++ aulas/aula148/varias_linhas.py
@@ -0,0 +1,14 @@
+"""O que seu módulo faz
+
+Lorem ipsum dolor sit amet. Rem adipisci neque est asperiores voluptate est sint
+eligendi qui ipsum debitis vel assumenda tempore nam consequatur architecto. At 
+veniam architecto ea quam fugiat qui odio sint? Et tempore excepturi ut volupta-
+tem quisquam vel numquam optio in sapiente totam est galisum facilis.
+
+Et placeat maiores non dicta accusamus vel dicta autem in voluptate fugiat. Sit 
+quibusdam tempore sit corrupti explicabo et voluptate voluptas.
+
+Ut incidunt recusandae quo dignissimos eveniet est galisum facilis et enim porro
+. Nam temporibus alias aut molestiae atque et internos accusamus qui perspiciat-
+is odio et nesciunt officiis ut atque animi.
+"""
