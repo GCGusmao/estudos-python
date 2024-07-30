a9995221827503f3ca2d75428885f40d57322b76
GCGusmao <69112221+GCGusmao@users.noreply.github.com>
2024-07-30 19:26:05 -0300

    andamento curso.

--- aulas/aula178/main.py
+++ aulas/aula178/main.py
@@ -6,8 +6,13 @@
 import pymysql
 
 connection = pymysql.connect(
-    host='',
-    user='',
-    password='',
-    database='',
+    host='localhost',
+    user='usuario',
+    password='senha',
+    database='base_de_dados',
 )
+
+with connection:
+    with connection.cursor() as cursor:
+        # SQL
+        print(cursor)
