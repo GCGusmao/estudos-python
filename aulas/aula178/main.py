683d16fc3d8143c14a5fadeaaf563a081f148a41
GCGusmao <69112221+GCGusmao@users.noreply.github.com>
2024-07-30 19:19:43 -0300

    Andamento curso

---
+++ aulas/aula178/main.py
@@ -0,0 +1,13 @@
+# PyMySQL - um cliente MySQL feito em Python Puro
+# Doc: https://pymysql.readthedocs.io/en/latest/
+# Pypy: https://pypi.org/project/pymysql/
+# GitHub: https://github.com/PyMySQL/PyMySQL
+
+import pymysql
+
+connection = pymysql.connect(
+    host='',
+    user='',
+    password='',
+    database='',
+)
